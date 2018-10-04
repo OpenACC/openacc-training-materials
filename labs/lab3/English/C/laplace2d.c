a36ff95604751dc583f9f23dabdfda865570537b
Jeff Larkin <jefflarkin@gmail.com>
2018-10-04 10:53:05 -0400

    Updated lab3 for online course

--- labs/lab3/English/C/laplace2d.c
+++ labs/lab3/English/C/laplace2d.c
@@ -39,18 +39,15 @@ void initialize(double *restrict A, double *restrict Anew, int m, int n)
         A[i] = 1.0;
         Anew[i] = 1.0;
     }
-    
-    #pragma acc enter data copyin(A[:m*n],Anew[:m*n])
 
 }
 
 double calcNext(double *restrict A, double *restrict Anew, int m, int n)
 {
     double error = 0.0;
-    #pragma acc parallel loop present(A,Anew)
+    #pragma acc parallel loop reduction(max:error) copyin(A[:n*m]) copyout(Anew[:n*m])
     for( int j = 1; j < n-1; j++)
     {
-        #pragma acc loop
         for( int i = 1; i < m-1; i++ )
         {
             Anew[OFFSET(j, i, m)] = 0.25 * ( A[OFFSET(j, i+1, m)] + A[OFFSET(j, i-1, m)]
@@ -63,10 +60,9 @@ double calcNext(double *restrict A, double *restrict Anew, int m, int n)
         
 void swap(double *restrict A, double *restrict Anew, int m, int n)
 {
-    #pragma acc parallel loop present(A,Anew)
+    #pragma acc parallel loop copyin(Anew[:n*m]) copyout(A[:n*m])
     for( int j = 1; j < n-1; j++)
     {
-        #pragma acc loop
         for( int i = 1; i < m-1; i++ )
         {
             A[OFFSET(j, i, m)] = Anew[OFFSET(j, i, m)];    
@@ -76,8 +72,6 @@ void swap(double *restrict A, double *restrict Anew, int m, int n)
 
 void deallocate(double *restrict A, double *restrict Anew)
 {
-    #pragma acc exit data delete(A,Anew)
-    
     free(A);
     free(Anew);
 }
