c5da4fc5b0f805d67390dc5d479efd0fae8feee8
efwright <efwright@udel.edu>
2019-02-13 15:46:31 -0500

    Lab9 C

--- labs/module9/C/OpenMP/Solution/filter.c
+++ labs/module9/C/OpenMP/Solution/filter.c
@@ -4,7 +4,7 @@
 #define MAX(X,Y) ((X>Y) ? X:Y)
 #define MIN(X,Y) ((X<Y) ? X:Y)
 
-void blur5_openmp(unsigned restrict char *imgData, unsigned restrict char *out, long w, long h, long ch)
+void blur5_openmp(unsigned char *imgData, unsigned char *out, long w, long h, long ch)
 {
   long step = w*ch;
   long x, y;
@@ -22,7 +22,6 @@ void blur5_openmp(unsigned restrict char *imgData, unsigned restrict char *out, 
   double scale = 1.0 / 35.0;
 
   int ndevices = acc_get_num_devices(acc_device_default);
-  printf("Found %d devices\n", ndevices);
   long rows_per_device = (h+(ndevices-1))/ndevices;
 
 #pragma omp parallel for num_threads(ndevices)
@@ -32,14 +31,12 @@ void blur5_openmp(unsigned restrict char *imgData, unsigned restrict char *out, 
     long copyLower = MAX(lower-(filtersize/2), 0);
     long copyUpper = MIN(upper+(filtersize/2), h);
 
-    acc_set_device_num(device, acc_device_default);
-
-#pragma acc declare copyin(filter)
     int tid = omp_get_thread_num();
-    printf("Thread %d running on device %d\n", tid, device);
+    acc_set_device_num(tid, acc_device_default);
+
 #pragma acc parallel loop \
- copyin(imgData[copyLower*step:(copyUpper-copyLower)*step]) \
- copyout(out[lower*step:(upper-lower)*step]) present(filter)
+ copyin(imgData[copyLower*step:(copyUpper-copyLower)*step], filter) \
+ copyout(out[lower*step:(upper-lower)*step])
     for(y = lower; y < upper; y++) {
 #pragma acc loop
       for(x = 0; x < w; x++) {
