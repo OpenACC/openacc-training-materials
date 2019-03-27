22b745e6aa8ed2eaf0a483fee3e644888867adf4
Jeff Larkin <jefflarkin@gmail.com>
2019-03-27 16:09:21 -0400

    Housekeeping

--- labs/module7/English/Fortran/main.cpp
+++ labs/module7/English/Fortran/main.cpp
@@ -33,19 +33,17 @@ int main(int argc, char** argv)
   unsigned char* output3 = new unsigned char[w*h*ch];
 
   // Warm Up
+  double st = omp_get_wtime();
+  blur5_serial(data, output1, w, h, ch);
+  printf("Time taken for serial blur5: %.4f seconds\n", omp_get_wtime()-st);
   blur5_pipelined(data, output3, w, h, ch);
 
-  double st = omp_get_wtime();
-  blur5(data, output1, w, h, ch);
+  st = omp_get_wtime();
+  blur5(data, output2, w, h, ch);
   printf("Time taken for parallel blur5: %.4f seconds\n", omp_get_wtime()-st);
 
-  printf("Running serial and baseline parallel for comparison...\n");
   st = omp_get_wtime();
-  blur5_serial(data, output2, w, h, ch);
-  printf("Time taken for serial blur5: %.4f seconds\n", omp_get_wtime()-st);
-
-  st = omp_get_wtime();
-  blur5_parallel(data, output2, w, h, ch);
+  blur5_parallel(data, output3, w, h, ch);
   printf("Time taken for baseline parallel blur5: %.4f seconds\n", omp_get_wtime()-st);
 
   printf("Checking results for comparison...\n");
@@ -64,7 +62,7 @@ int main(int argc, char** argv)
     printf("Code results are correct.\n");
   }
 
-  memcpy(data, output1, w*h*ch);
+  memcpy(data, output2, w*h*ch*sizeof(unsigned char));
   writeImage(argv[2]);
 
   delete[] output1;
