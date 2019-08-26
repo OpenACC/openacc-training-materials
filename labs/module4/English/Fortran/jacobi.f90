eb3293c225afac62e3acdae21c4b39745f873668
Jeff Larkin <jefflarkin@gmail.com>
2019-08-26 13:21:20 -0400

    Updated license to APL 2.0

--- labs/module4/English/Fortran/jacobi.f90
+++ labs/module4/English/Fortran/jacobi.f90
@@ -1,32 +1,20 @@
-! Copyright (c) 2012, NVIDIA CORPORATION. All rights reserved.
 !
-! Redistribution and use in source and binary forms, with or without
-! modification, are permitted provided that the following conditions
-! are met:
-!  * Redistributions of source code must retain the above copyright
-!    notice, this list of conditions and the following disclaimer.
-!  * Redistributions in binary form must reproduce the above copyright
-!    notice, this list of conditions and the following disclaimer in the
-!    documentation and/or other materials provided with the distribution.
-!  * Neither the name of NVIDIA CORPORATION nor the names of its
-!    contributors may be used to endorse or promote products derived
-!    from this software without specific prior written permission.
+! Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
 !
-! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
-! EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-! PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-! CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-! EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-! PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-! OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-! (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+! Licensed under the Apache License, Version 2.0 (the "License");
+! you may not use this file except in compliance with the License.
+! You may obtain a copy of the License at
+!
+!     http://www.apache.org/licenses/LICENSE-2.0
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
+!
 program jacobi
   use laplace2d
-  use nvtx_mod
   implicit none
   integer, parameter :: fp_kind=kind(1.0d0)
   integer, parameter :: n=4096, m=4096, iter_max=1000
@@ -54,15 +42,11 @@ program jacobi
   
   do while ( error .gt. tol .and. iter .lt. iter_max )
 
-    call nvtxstartrange('calc')
     error = calcNext(A, Anew, m, n)
-    call nvtxendrange()
-    call nvtxstartrange('swap')
     call swap(A, Anew, m, n)
-    call nvtxendrange()
 
     if(mod(iter,100).eq.0 ) write(*,'(i5,f10.6)'), iter, error
-	
+
     iter = iter + 1
 
   end do
@@ -70,5 +54,5 @@ program jacobi
   call cpu_time(stop_time) 
   write(*,'(a,f10.3,a)')  ' completed in ', stop_time-start_time, ' seconds'
 
-  ! deallocate (A,Anew)
+  deallocate (A,Anew)
 end program jacobi
