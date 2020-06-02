e73e283faa0f349296d8a4c72898cd9f22d961d0
Jeff Larkin <jefflarkin@gmail.com>
2020-06-02 15:28:56 -0400

    Postprocessor now changes .ipynb to .md

--- scripts/lab_postprocessor.py
+++ scripts/lab_postprocessor.py
@@ -6,6 +6,7 @@ class LabPostProcessor(PostProcessorBase):
         data = f.read()
         f.close()
         output = data.replace("```python\n!","```bash\n$ ")
+        output = output.replace(".ipynb",".md")
         f = open(fileName, "w")
         f.write(output)
         f.close()
